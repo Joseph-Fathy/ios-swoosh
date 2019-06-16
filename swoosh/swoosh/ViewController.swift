db82afb3b636790111db2760968b4a1d81884f78
jo <joseph.gayed@vodafone.com>
2019-06-17 00:46:42 +0200

    adjust layout position and size programmatically to fit multiple screens

--- swoosh/swoosh/ViewController.swift
+++ swoosh/swoosh/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //reposition the logo to ceneter horizontally
+        swoosh.frame=CGRect(x: (view.frame.width/2)-(swoosh.frame.width/2),
+                            y: 50.0, width: swoosh.frame.width, height: swoosh.frame.height)
+        
+        //make the bagrounnd image fill the screen
+        bgImg.frame=view.frame
+        
     }
 
 

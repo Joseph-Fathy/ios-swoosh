2d8ee5bc29045c96fb12ca7a7fb2cad8c1cb806b
jo <joseph.gayed@vodafone.com>
2019-06-19 18:41:29 +0200

    refactor project structure to mvc folders , create view controller classes and connect them with storyboard , activate back buttons in league and levels screens

---
+++ swoosh/swoosh/controller/LeagueVC.swift
@@ -0,0 +1,22 @@
+//
+//  LeagueVC.swift
+//  swoosh
+//
+//  Created by Joe on 6/19/19.
+//  Copyright © 2019 Joe. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToLeague(unwindSegueToLeague: UIStoryboardSegue) {
+    }
+
+}
--- swoosh/swoosh/controller/WelcomeVC.swift
+++ swoosh/swoosh/controller/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
     
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    @IBAction func unwindFromSkillVC(unWindSegue:UIStoryboardSegue){
+    @IBAction func unwindToWelcome(unWindSegueToWelcome:UIStoryboardSegue){
         
     }
 

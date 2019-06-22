0d47effcc70a28e3c8c77d9c4dd67d0c6c22b4c1
jo <joseph.gayed@vodafone.com>
2019-06-22 12:03:57 +0200

    adding FinalVC , updating background colors according to user selections , adding constants values in player )

--- swoosh/swoosh/controller/LeagueVC.swift
+++ swoosh/swoosh/controller/LeagueVC.swift
@@ -10,13 +10,47 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player:Player!
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+//VC functions =====================
+    //onCreate
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        player = Player()
     }
-    
+    //onBackPressed
     @IBAction func unwindToLeague(unwindSegueToLeague: UIStoryboardSegue) {
     }
-
+    //this method is called before viewDidLoad method of the destination
+    //used to pass data between VCs 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //if the segue is to open Levels ViewContoller
+        if let levelsVC = segue.destination as? LevelsVc {
+            levelsVC.player = player
+        }
+    }
+//hanndle buttons =====================
+    @IBAction func onMenTapped(_ sender: Any) {
+        selectLeague(league: Player.LEAGUE_MEN)
+    }
+    
+    @IBAction func onWomenTapped(_ sender: Any) {
+        selectLeague(league: Player.LEAGUE_WOMEN)
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(league: Player.LEAGUE_COED)
+    }
+    
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "LevelsVCSegue", sender: self)
+    }
+    
+    func selectLeague(league:String){
+        player.desiredLeague=league
+        nextBtn.isEnabled = true
+    }
+    
 }
---
+++ swoosh/swoosh/model/Player.swift
@@ -0,0 +1,21 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by Joe on 6/22/19.
+//  Copyright © 2019 Joe. All rights reserved.
+//
+
+import Foundation
+struct Player
+{
+    var desiredLeague:String?
+    var level:String?
+    
+    static let LEAGUE_MEN = "Men"
+    static let LEAGUE_WOMEN = "Women"
+    static let LEAGUE_COED = "Co-ed"
+    
+    static let LEVEL_BIGENNER = "Beginner"
+    static let LEVEL_BALLER = "Baller"
+}
